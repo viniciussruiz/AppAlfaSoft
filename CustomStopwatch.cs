7f6a56ac0ef09a8449a9dbbba666c56a464cdad5
viniciussruiz <71984074+viniciussruiz@users.noreply.github.com>
2022-01-23 16:55:51 -0300

    Uploading project files

---
+++ CustomStopwatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAlfaSoft
+{
+    public class CustomStopwatch : Stopwatch
+    {
+
+        public DateTime? StartAt { get; private set; }
+        public DateTime? EndAt { get; private set; }
+
+
+        public new void Start()
+        {
+            StartAt = DateTime.Now;
+
+            base.Start();
+        }
+
+        public new void Stop()
+        {
+            EndAt = DateTime.Now;
+
+            base.Stop();
+        }
+
+        public new void Reset()
+        {
+            StartAt = null;
+            EndAt = null;
+
+            base.Reset();
+        }
+
+        public new void Restart()
+        {
+            StartAt = DateTime.Now;
+            EndAt = null;
+
+            base.Restart();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppAlfaSoft;
+using System.Net.Http.Headers;
+
+Console.Write("Enter the file path: ");
+
+string path = Console.ReadLine();
+
+string[] names = File.ReadAllLines(path);
+CustomStopwatch sw = new();
+
+foreach (var name in names)
+{
+    Console.WriteLine("User: " + name);
+    CallApi(name);
+    
+}
+
+Thread.Sleep(5000);
+
+void CallApi(string nome)
+{
+    
+    using var client = new HttpClient();
+    client.BaseAddress = new Uri("https://api.bitbucket.org/2.0/users/");
+    client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+    client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+    var url = client.BaseAddress + nome + "/";
+    Console.WriteLine("Calling Url: " + url);
+
+    var response = client.GetAsync(nome + "/").Result;
+    var resp = response.Content.ReadAsStringAsync().Result;
+
+    Console.WriteLine("Response: " + resp);
+    SaveLog(resp, nome);
+}
+
+void SaveLog(string response, string name)
+{
+    var path = String.Concat(Environment.CurrentDirectory, "\\Log.txt");
+
+    FileStream fs = new(path,
+                FileMode.Append);
+
+    StreamWriter sw = new(fs);
+    sw.WriteLine("User: " + name);
+    sw.WriteLine("Date: " + DateTime.Now.ToString());
+    sw.WriteLine("Respose" + response);
+    sw.Flush();
+    sw.Close();
+    fs.Close();
+}
+
+
+
